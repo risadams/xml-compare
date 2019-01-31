eef397c2dfb8f39f118e66c334e31196654203b2
Chris Adams <chris@tuesdaybegins.com>
2019-01-31 09:41:19 -0500

    Adding source files

---
+++ XmlCompare/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CommandLine;
+
+namespace XmlCompare
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<CommandArguments>(args)
+                .WithParsed(cmd => { })
+                .WithNotParsed(err =>
+                {
+                    Console.WriteLine("Error parsing arguments:");
+                    foreach (var error in err) Console.WriteLine($"\t{error.ToString()}");
+                });
+        }
+    }
+
+    public class CommandArguments
+    {
+        [Option('s', "source", HelpText = "Sets the source file for comparison", Required = true)]
+        public string SourceFile { get; set; }
+
+        [Option('t', "target", HelpText = "Sets the target file for comparison", Required = true)]
+        public string TargetFile { get; set; }
+
+
+        [Option('o', "out", HelpText = "If specified, the XML DiffGram will be saved to the target location.")]
+        public string DiffgramOutput { get; set; }
+    }
+}
---
+++ XmlCompare/XmlDiffGramGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using System.Xml;
+using Microsoft.XmlDiffPatch;
+
+namespace XmlCompare
+{
+    public class XmlDiffGramGenerator
+    {
+        public static bool Comapare(string sourcePath, string targetPath, out string diffGram)
+        {
+            var dg = new StringBuilder();
+            var diff = new XmlDiff(XmlDiffOptions.IgnoreChildOrder | XmlDiffOptions.IgnoreWhitespace | XmlDiffOptions.IgnoreComments | XmlDiffOptions.IgnorePrefixes);
+
+            bool same;
+            using (var diffGramWriter = XmlWriter.Create(dg))
+            {
+                same = diff.Compare(sourcePath, targetPath, false, diffGramWriter);
+            }
+
+            diffGram = dg.ToString();
+
+            return same;
+        }
+    }
+}
